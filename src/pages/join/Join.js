db40e576e24295e5550fdcb6c3aebc23932851dd
Arafath Miah <miah.arafath@gmail.com>
2021-10-25 23:54:40 +0100

    added animated bg to join page

--- src/pages/join/Join.js
+++ src/pages/join/Join.js
@@ -1,7 +1,15 @@
+import Particles from "react-particles-js";
+import particlesConfig from "../../config/particlesConfig";
+
+import "./Join.css";
+
 export default function Join() {
+
+    console.log(particlesConfig);
+
     return (
         <div className="Join">
-            This is the join page
+            <Particles className="particles" height="100vh" params={particlesConfig} />
         </div>
     );
 };
