8d18bd608b12d6cf0448f9559f322e46104e68c7
velinkalenderski <velinkalenderski@hotmail.co.uk>
2021-10-26 14:35:28 +0100

    title animation added

--- src/components/UI/Title/Title.js
+++ src/components/UI/Title/Title.js
@@ -1,11 +1,12 @@
 import React from "react";
 import squidFont from '../../assets/SquizGame.png'
+import squidAnimation from '../../assets/SquizGameCropped_1.gif'
 import './title.css'
 
 const Title = () => {
   return (
     <div>
-      <img src={squidFont} alt="SQUIZ GAME" />
+      <img src={squidAnimation} alt="SQUIZ GAME" />
     </div>
   );
 };
