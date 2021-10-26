c2e300aa7c432ba5be430d51f9c3a7d14ee40fc3
velinkalenderski <velinkalenderski@hotmail.co.uk>
2021-10-26 18:35:56 +0100

    feature trials

--- src/App.js
+++ src/App.js
@@ -7,22 +7,32 @@ import NotFound from "./pages/404/NotFound";
 import JoinRoom from "./pages/JoinRoom/JoinRoom";
 import CreateRoom from "./pages/createRoom/CreateRoom";
 import GameRoom from "./pages/gameRoom/GameRoom"
+import { useContext } from "react"
+import { SocketContext } from "./Context"
+import { io } from "socket.io-client";
 
 function App() {
+
+  const socket = io("http://localhost:4000/");
+
+  socket.on("created", (roomId) => {
+    console.log(roomId);
+  });
+
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={Home} />
 
-        <Route exact path="/create" component={CreateRoom} />
-
-        <Route exact path="/game/:id" component={GameRoom} />
+        <SocketContext.Provider value={socket}>
+          <Route exact path="/create" component={CreateRoom} />
+          <Route exact path="/game/:id" component={GameRoom} />
+          <Route exact path="/join" component={JoinRoom} />
+        </SocketContext.Provider>
 
         <Route exact path="/leaderboard" component={Leaderboard} />
 
-        <Route exact path="/join" component={JoinRoom} />
 
-        <Route exact path="/create" component={CreateRoom} />
 
         <Route render={() => <NotFound />} />
       </Switch>
